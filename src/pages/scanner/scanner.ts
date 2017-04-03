0ac24361f04ead6e446b26cba07db5d8eef598f5
jchuahtacc <jchuah@tacc.utexas.edu>
2017-04-03 09:23:16 -0500

    Initial push

---
+++ src/pages/scanner/scanner.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-scanner',
+  templateUrl: 'scanner.html'
+})
+export class Scanner {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
